b6f6c1e00907c5eea050b649e088c99e3ebab289
Tydus <Tydus@Tydus.org>
2018-09-10 00:56:23 +0900

    Fix bugs

--- splitfs.py
+++ splitfs.py
@@ -3,11 +3,13 @@
 import re
 import os
 import sys
+import stat
+import logging
 import os.path
 import threading
 import functools
 
-from fuse import FUSE, FuseOSError, Operations
+from fuse import FUSE, FuseOSError, Operations, LoggingMixIn
 
 DEFAULT_CHUNK_SIZE = '650MB'
 
@@ -22,41 +24,56 @@ def parseSize(size_str):
 
 def refresh_stat(func):
     @functools.wraps(func)
-    def wrapped(self, piece):
-        self.src_stat = os.fstat(self.src_fd)
+    def wrapped(self, *args, **kwargs):
+        st = os.fstat(self.src_fd)
+        self.src_stat = dict((key, getattr(st, key)) for key in (
+            'st_atime', 'st_ctime', 'st_mtime', 'st_size',
+            'st_uid', 'st_gid', 'st_mode', 'st_nlink',
+        ))
         return func(self, *args, **kwargs)
+    return wrapped
 
-class SplitFS(Operations):
+class SplitFS(LoggingMixIn, Operations):
     def __init__(self, src, chunk_size):
         self.src_fd = os.open(src, os.O_RDONLY)
-        self.src_stat = os.fstat(self.src_fd)
-        self.src_name = os.basename(src)
+        self.src_stat = None
+        self.src_name = os.path.basename(src)
         self.src_lock = threading.Lock()
 
         self.chunk_size = parseSize(chunk_size)
 
+        print "self.chunk_size = %d" % self.chunk_size
+
     @refresh_stat
     def get_piece_range(self, n):
-        src_size = self.src_stat.st_size
-        start = n * chunk_size
+        src_size = self.src_stat['st_size']
+        start = n * self.chunk_size
         assert start < src_size
-        end = min((n + 1) * chunk_size, src_size) - 1
+        end = min((n + 1) * self.chunk_size, src_size) - 1
         size = end - start + 1
         return (start, end, size)
 
     @refresh_stat
     def getattr(self, path, fh=None):
-        n = int(path.rsplit('.', 2)[-1])
-        stat = self.src_stat.copy()
-        _, _, size = self.get_piece_range(n)
-        stat['st_size'] = size
-        return start
+        #import pdb; pdb.set_trace()
+        st = self.src_stat.copy()
+
+        if path[-1] == '/':
+            st['st_size'] = 4096
+            st['st_nlink'] = 2
+            st['st_mode'] = (stat.S_IFDIR | 0o777)
+        else:
+            n = int(path.rsplit('.', 2)[-1])
+            _, _, size = self.get_piece_range(n)
+            st['st_size'] = size
+
+        return st
 
     @refresh_stat
     def readdir(self, path, fh):
         print "readdir: path = " + path
         
-        npieces = -(-self.src_stat.st_size / self.chunk_size) # safe ceiling div
+        npieces = -(-self.src_stat['st_size'] / self.chunk_size) # safe ceiling div
 
         return ['.', '..'] + ['%s.%d' % (self.src_name, i) for i in xrange(npieces)]
 
@@ -73,14 +90,18 @@ class SplitFS(Operations):
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        print "Usage: %s <source> <mountpoint> [chunksize]"
+        print "Usage: %s <source> <mountpoint> [chunksize]" % sys.argv[0]
         exit(-1)
 
-    FUSE(SplitFS(
-        sys.argv[1],
-        sys.argv.get(3, DEFAULT_CHUNK_SIZE),
+    logging.basicConfig(level=logging.DEBUG)
+
+    FUSE(
+        SplitFS(
+            sys.argv[1],
+            sys.argv[3] if len(sys.argv) == 4 else DEFAULT_CHUNK_SIZE,
+        ),
         sys.argv[2],
         foreground=True,
-        raw_fi = False,
-    ))
+        raw_fi=False,
+    )
 
