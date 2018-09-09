a1b6b837e31543902da43e0e31d90a6ce40fb5a1
Tydus <Tydus@Tydus.org>
2018-09-10 03:05:06 +0900

    Support block file and symlinks

--- splitfs.py
+++ splitfs.py
@@ -5,6 +5,8 @@ import os
 import sys
 import stat
 import errno
+import fcntl
+import struct
 import logging
 import os.path
 import threading
@@ -23,33 +25,57 @@ def parseSize(size_str):
 
     return int(n) * mult
 
+def getblocksize(path):
+    req = 0x80081272 # BLKGETSIZE64 on Linux
+    buf = ' ' * 8
+    with open(path) as dev:
+        buf = fcntl.ioctl(dev.fileno(), req, buf)
+    return struct.unpack('L', buf)[0]
+
+# XXX: LOCK it
 def refresh_stat(func):
     @functools.wraps(func)
     def wrapped(self, *args, **kwargs):
-        st = os.fstat(self.src_fd)
+        path = self.src_path
+        for i in xrange(10):
+            st = os.lstat(path)
+            if stat.S_ISBLK(st.st_mode):
+                size = getblocksize(path)
+                if size == 0: raise FuseOSError(errno.ENOENT)
+                mode = (stat.S_IFREG | stat.S_IMODE(st.st_mode))
+                break
+            elif stat.S_ISREG(st.st_mode):
+                size = st.st_size
+                mode = st.st_mode
+                break
+            elif stat.S_ISLNK(st.st_mode):
+                path = os.readlink(path)
+            else:
+                #raise FuseOSError(errno.EACCES)
+                raise FuseOSError(errno.ENOENT)
+        else:
+            print "Warning: following link for too much time, aborted."
+            raise FuseOSError(errno.ENOENT)
+
         self.src_stat = dict((key, getattr(st, key)) for key in (
-            'st_atime', 'st_ctime', 'st_mtime', 'st_size',
-            'st_uid', 'st_gid', 'st_mode', 'st_nlink',
+            'st_atime', 'st_ctime', 'st_mtime',
+            'st_uid', 'st_gid', 'st_nlink',
         ))
+        self.src_stat['st_size'] = size
+        self.src_stat['st_mode'] = mode
         return func(self, *args, **kwargs)
     return wrapped
 
 
 class SplitFS(LoggingMixIn, Operations):
     def __init__(self, src, chunk_size):
-        self.src_fd = os.open(src, os.O_RDONLY)
         self.src_stat = None
+        self.src_path = src
         self.src_name = os.path.basename(src)
-        self.src_lock = threading.Lock()
 
         self.chunk_size = parseSize(chunk_size)
 
         print "self.chunk_size = %d" % self.chunk_size
-
-    def return_rofs(self, *args, **kwargs):
-        raise FuseOSError(errno.EROFS)
-
-    utimens = return_rofs
 
     def get_piece_range(self, n):
         src_size = self.src_stat['st_size']
@@ -58,6 +84,11 @@ class SplitFS(LoggingMixIn, Operations):
         end = min((n + 1) * self.chunk_size, src_size) - 1
         size = end - start + 1
         return (start, end, size)
+
+    # access() should make sure the source file exists
+    @refresh_stat
+    def access(self, path, mode):
+        return 0
 
     def get_n(self, path):
         fn = os.path.basename(path)
@@ -81,7 +112,7 @@ class SplitFS(LoggingMixIn, Operations):
         if path[-1] == '/':
             st['st_nlink'] = 2
             st['st_size'] = 4096
-            st['st_mode'] = (stat.S_IFDIR | (st['st_mode'] & 0o777 | 0o111))
+            st['st_mode'] = (stat.S_IFDIR | stat.S_IMODE(st['st_mode']) | 0o111)
         else:
             n = self.get_n(path)
             _, _, size = self.get_piece_range(n)
@@ -92,6 +123,16 @@ class SplitFS(LoggingMixIn, Operations):
     def open(self, path, flags):
         if flags & (os.O_RDWR | os.O_WRONLY):
             raise FuseOSError(errno.EROFS)
+
+        # Just checking the path is valid
+        self.get_n(path)
+
+        fh = os.open(self.src_path, flags)
+        print "open: %s => %d" % (path, fh)
+        return fh
+
+    def release(self, path, fh):
+        os.close(fh)
         return 0
 
     @refresh_stat
@@ -108,11 +149,10 @@ class SplitFS(LoggingMixIn, Operations):
 
         start, end, piece_size = self.get_piece_range(n)
         #assert offset + size <= piece_size
-        print "read: [start, end]"
+        #print "read: fh=%d size=%d offset=%d => [%s, %s]" % (fh, size, offset, start, end)
 
-        with self.src_lock:
-            os.lseek(self.src_fd, start + offset, 0)
-            return os.read(self.src_fd, size)
+        os.lseek(fh, start + offset, 0)
+        return os.read(fh, size)
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
@@ -128,6 +168,5 @@ if __name__ == "__main__":
         ),
         sys.argv[2],
         foreground=True,
-        raw_fi=False,
     )
 
